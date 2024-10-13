43d0d014e095afa27ad0403d2a58ffdfaf7b03df
Chad Green <chadgreen@chadgreen.com>
2024-10-12 20:16:29 -0400

    Removing output for workspace identifier.

--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,3 @@ output "application_insights" {
   value = azurerm_application_insights.target
   description = "The managed Application Insights resource."
 }
-
-output "workspace_id" {
-  value = local.workspace_id
-  description = "The ID of the Log Analytics Workspace associated with the Application Insights resource."
-}
